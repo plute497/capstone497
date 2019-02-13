bdf5b950babe887afaf4d2fb2dec717b45ff45a4
card-b <adam.m.denny2@gmail.com>
2019-02-12 22:22:59 -0800

    got AR view elements working on Android

--- components/ar/ar-view.js
+++ components/ar/ar-view.js
@@ -11,11 +11,13 @@ import {
     ViroText,
     ViroConstants,
     ViroARSceneNavigator,
+    ViroAmbientLight,
     ViroSphere,
     ViroMaterials,
     ViroOmniLight,
     ViroBox,
-    ViroARPlane
+    ViroARPlane,
+    ViroDirectionalLight
 } from 'react-viro';
 
 let api = "3D8F23E6-792C-4F66-B078-94EDB6E33A32";
@@ -53,15 +55,41 @@ export default class ArView extends Component {
     }
 }
 
-export function ArScene() {
-    return (
-        <ViroARScene style={{ flex: 1 }} onTrackingUpdated={this._onInitialized} >
-            <ViroText text={"hello world3"} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-            <ViroARPlane>
-                <ViroBox position={[0, .5, 0]} />
-            </ViroARPlane>
-        </ViroARScene>
-    )
+export class ArScene extends Component {
+    state = {
+        text: "Loading"
+    }
+
+    _onInitialized = (state, reason) => {
+        console.log('initialized');
+        console.log(state, reason);
+        if (state == ViroConstants.TRACKING_NORMAL) {
+            this.setState({
+                text: "Hello World!"
+            });
+        } else if (state == ViroConstants.TRACKING_NONE) {
+            // Handle loss of tracking
+        }
+    }
+
+    render() {
+        return (
+            <ViroARScene style={{ flex: 1 }} onTrackingUpdated={this._onInitialized} >
+                <ViroDirectionalLight
+                    color="#ffffff"
+                    direction={[0, -1, 0]}
+                />
+                <ViroAmbientLight color="#ffffff" intensity={100} />
+                <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+                <ViroBox materials={["grid"]} position={[0, .5, -2]} />
+                <ViroSphere
+                    position={[1, 0.5, -2]}
+                    heightSegmentCount={20}
+                    widthSegmentCount={20}
+                    radius={0.5} materials={["grid"]} />
+            </ViroARScene>
+        )
+    }
 }
 
 ViroMaterials.createMaterials({
